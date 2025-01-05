2f2a815d20897cd9be317951959d9a01eb9457f4
yosephbernandus <yosephbernandus@gmail.com>
2025-01-05 09:58:35 +0700

    chore: prometheus envar config fixing

--- observability/prometheus/prometheus-entrypoint.sh
+++ observability/prometheus/prometheus-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-envsubst < /etc/prometheus/prometheus.yml.template > /etc/prometheus/prometheus.yml
+sed -e "s|\${PROMETHEUS_ADDR}|$PROMETHEUS_ADDR|g" \
+    -e "s|\${DJANGO_GRPC_ADDR}|$DJANGO_GRPC_ADDR|g" \
+    -e "s|\${NODE_EXPORTER_ADDR}|$NODE_EXPORTER_ADDR|g" \
+    /etc/prometheus/prometheus.yml.template > /etc/prometheus/prometheus.yml
+
 exec prometheus \
     --config.file=/etc/prometheus/prometheus.yml \
     --storage.tsdb.path=/prometheus \
